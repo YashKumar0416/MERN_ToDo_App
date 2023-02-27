ebc19d859f82add2aeea771041c153f9a4877494
YashKumar0416 <122213030+YashKumar0416@users.noreply.github.com>
2023-02-28 03:14:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors);
 const app = express();
 require('./db/db');
 const User = require('./Model/User');
 const PORT = process.env.PORT || 8000;
 
+app.use(cors())
 app.use(express.json())
 app.use(require('./Router/routes'))
 
